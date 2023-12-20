36d056f5fb6a463c53f97c754831f14de4c1cdd2
Moivvas <130219729+Moivvas@users.noreply.github.com>
2023-12-20 12:00:56 +0200

    main proc req

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pathlib
-from fastapi import FastAPI, Request
+from sqlalchemy import text
+from fastapi import FastAPI, Request, HTTPException, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 
+from src.database.db import get_db
 from src.routes import (auth,
                         users,
                         tags,
@@ -33,6 +36,19 @@ app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 async def root(request: Request):
     return templates.TemplateResponse('index.html', {"request": request, "title": "FAST_image_App"})
 
+@app.get("/api/healthchecker")
+def healthchecker(db: Session = Depends(get_db)):
+    try:
+        # Make request
+        result = db.execute(text("SELECT 1")).fetchone()
+        if result is None:
+            raise HTTPException(
+                status_code=500, detail="Database is not configured correctly"
+            )
+        return {"message": "Welcome to FastAPI!"}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Error connecting to the database")
 
 app.include_router(auth.router, prefix='/project')
 app.include_router(users.router, prefix='/project')
